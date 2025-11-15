67250705be3c5e831b2d8ef1a3edea02db854fb2
M-Polonczyk <m.polonczyk@outlook.com>
2025-11-15 20:06:01 +0100

    evaluate

--- src/run_prediction.py
+++ src/run_prediction.py
@@ -6,19 +6,11 @@ import torch
 
 sys.path.insert(0, str(Path(__file__).parent.parent))
 from gnn_vuln_detection.data_processing.graph_converter import DataclassToGraphConverter
+from gnn_vuln_detection.dataset import DiverseVulDatasetLoader
 from gnn_vuln_detection.models.factory import (
     GNNModelFactory,
 )
 from gnn_vuln_detection.utils import config_loader
-from src.gnn_vuln_detection.dataset import DiverseVulDatasetLoader
-
-
-def get_random_code_sample(dataset_config, labels):
-    diversevul_loader = DiverseVulDatasetLoader(
-        dataset_path=dataset_config["diversevul"]["dataset_path"],
-    )
-    samples = diversevul_loader.load_dataset(labels)
-    return random.choice(samples)
 
 
 def main():
@@ -33,6 +25,7 @@ def main():
     diversevul_loader = DiverseVulDatasetLoader(
         dataset_path=dataset_config["diversevul"]["dataset_path"],
     )
+    converter = DataclassToGraphConverter()
     samples = diversevul_loader.load_dataset([val["cwe_id"] for val in cwes])
 
     for _ in range(10):
@@ -48,7 +41,6 @@ def main():
         print(f"{code_sample.cwe_ids=}")
         print(f"{code_sample.cwe_ids_labeled=}\n")
 
-        converter = DataclassToGraphConverter()
         batch_graph = converter.code_sample_to_pyg_data(code_sample)
 
         input_dim = batch_graph.x.shape[1]
@@ -63,7 +55,11 @@ def main():
         )
         model.load_state_dict(torch.load("cwe_detector.pth"))
 
-        model.eval()
+        y_true, y_probs, y_labels = model.evaluate([batch_graph])
+        print("y_true:", y_true)
+        print("y_probs:", y_probs)
+        print("y_labels:", y_labels)
+
         with torch.no_grad():
             output = model(batch_graph.x, batch_graph.edge_index, batch_graph.batch)
 
